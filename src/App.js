8c7e9042fc755be140ca6d01938baccb3325aa51
Achilesseb <prisacariuvictor@gmail.com>
2022-08-01 11:11:08 +0300

    Solved React key errors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import "./App.css";
 import IntroPage from "./components/IntroPage/introPage";
-import PokemonCards from "./components/PokemonCards/pokemonCards";
+import PokemonCards from "./components/PokemonCards/PokemonCards";
 
 function App() {
   return (
@@ -11,12 +10,11 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<IntroPage />} />
-          <Route path="/battleground" element={<PokemonCards/>} />
+          <Route path="/battleground" element={<PokemonCards />} />
         </Routes>
       </Router>
     </div>
   );
-
 }
 
 export default App;
--- src/components/PokemonCards/PokemonCards.jsx
+++ src/components/PokemonCards/PokemonCards.jsx
@@ -1,58 +1,67 @@
-import './App.css';
 // import {pokemons} from './data/pokemons.js'
 // import {typeColor} from "./data/typeColor";
 // import {pokemonsFirst20} from "./data/pokemons-first20";
 
-import {Card} from './components/Card'
-import './styles/fonts.css'
-import {useEffect, useState} from "react";
-import {AnotherCard} from "./components/AnotherCard";
+import { Card } from "../Card";
+import "../../styles/fonts.css";
+import { useEffect, useState } from "react";
+import { AnotherCard } from "../AnotherCard.js";
 
 function PokemonCards() {
-    // pokemonsFirst20.forEach((pokemon) => pokemon.id = pokemon.url.slice(0, -1).split("/").pop())
+  // pokemonsFirst20.forEach((pokemon) => pokemon.id = pokemon.url.slice(0, -1).split("/").pop())
 
-    const [pokemons, setPokemons] = useState('')
+  const [pokemons, setPokemons] = useState([]);
 
-    useEffect(()=> {
-        (async (limit = 4) => {
-            const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=0`);
-            const json = await res.json();
-            json.results.forEach((pokemon) => pokemon.id = pokemon.url.slice(0, -1).split("/").pop())
-            //console.log(json.results);
-            setPokemons(json.results)
-        })();
-    }, [])
+  useEffect(() => {
+    (async (limit = 4) => {
+      const res = await fetch(
+        `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=0`
+      );
+      const json = await res.json();
+      json.results.forEach(
+        (pokemon) => (pokemon.id = pokemon.url.slice(0, -1).split("/").pop())
+      );
+      //console.log(json.results);
+      setPokemons(json.results);
+    })();
+  }, []);
 
-    const [x, setX] = useState('0')
-    const [y, setY] = useState('0')
-    // let x,y;
-    const pokeballMove = (e) => {
-        setX(`${e.clientX+30}px`)
-        setY(`${e.clientY+35}px`)
-        // console.log(x)
-        // x = `${e.clientX}px`
-        // y = `${e.clientY}px`
-    }
+  const [x, setX] = useState("0");
+  const [y, setY] = useState("0");
+  // let x,y;
+  const pokeballMove = (e) => {
+    setX(`${e.clientX + 30}px`);
+    setY(`${e.clientY + 35}px`);
+    // console.log(x)
+    // x = `${e.clientX}px`
+    // y = `${e.clientY}px`
+  };
+  console.log("Salut", pokemons);
+  return (
+    <div
+      onContextMenu={(e) => e.preventDefault()}
+      className="cards-container"
+      onMouseMove={(e) => pokeballMove(e)}
+    >
+      {
+        pokemons.length > 0 &&
+          pokemons.map((pokemon) => <Card pokemon={pokemon} key={pokemon.id} />)
+        // (<AnotherCard key={pokemon.id} pokemon={pokemon}/>))
+      }
+      {/* <div className="cards-container">
+        {pokemons &&
+          pokemons.map((pokemon) => (
+            <AnotherCard key={pokemon.id} pokemon={pokemon} />
+          ))}
+      </div> */}
 
-
-    return (
-        <div onContextMenu={(e)=> e.preventDefault()} className="cards-container" onMouseMove={(e)=>pokeballMove(e)}>
-            {
-                pokemons && pokemons.map((pokemon) =>
-                    (<Card key={pokemon.id} pokemon={pokemon}/>))
-                    // (<AnotherCard key={pokemon.id} pokemon={pokemon}/>))
-            }
-            <div className="cards-container">
-                {
-                    pokemons && pokemons.map((pokemon) =>
-                        (<AnotherCard key={pokemon.id} pokemon={pokemon}/>))
-                }
-            </div>
-
-            <div className="pokeball" style={{left:x, top:y}} >
-                <img  style={{width: 40, height:40}} src={require("./img/pokeball.png")}/>
-            </div>
-        </div>
-    )
-            }
+      <div className="pokeball" style={{ left: x, top: y }}>
+        <img
+          style={{ width: 40, height: 40 }}
+          src={require("../../img/pokeball.png")}
+        />
+      </div>
+    </div>
+  );
+}
 export default PokemonCards;
