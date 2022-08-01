539159d70df7fc051bfeb68c7d0d95b1b650d559
Achilesseb <prisacariuvictor@gmail.com>
2022-08-01 11:32:04 +0300

    Changed image sources and changed styles in PokemonCard

--- src/components/IntroPage/introPage.jsx
+++ src/components/IntroPage/introPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./introPage.css";
-import pokemonLogo from "./background-logo.png";
-import fightLogo from "./fight_logo.png";
+import pokemonLogo from "../../img/pokemon_logo.png";
+import fightLogo from "../../img/fight_logo.png";
 import { Link } from "react-router-dom";
 
 const IntroPage = () => {
