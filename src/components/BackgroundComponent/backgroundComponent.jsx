9907660eb2530921d6bdd70b566f998e38e4703b
Achilesseb <prisacariuvictor@gmail.com>
2022-08-03 13:32:47 +0300

    Changed styles for more responsive design

--- src/components/BackgroundComponent/backgroundComponent.jsx
+++ src/components/BackgroundComponent/backgroundComponent.jsx
@@ -3,7 +3,16 @@ import "./backGroundComponent.styles.css";
 import backGroundImage from "../../img/background.webp";
 const BackgroundImage = ({ image = backGroundImage }) => (
   <div className="background">
-    <img className="backgroundImage" src={image} />
+    {/* <img className="backgroundImage" src={image} /> */}
+    <div
+      style={{
+        backgroundImage: `url(${image}
+        )`,
+        height: "100%",
+        width: "100%",
+        backgroundSize: "100% 100%",
+      }}
+    ></div>
   </div>
 );
 export default BackgroundImage;
